b3d86b488cec414ff08eb42aba7660a8662cbd98
Renan Garcia <email@renangarcia.me>
2018-08-24 19:02:00 -0300

    Adicionado features

---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import TodoListItem from './TodoListItem';
+import { toggleTodo } from '../actions';
+
+const TodoList = ({ todos, dispatchToggleTodo }) => (
+  <View>
+    { todos.map( todo => 
+        <TodoListItem key={todo.id} todo={todo} onPressTodo={() => dispatchToggleTodo(todo.id)} />
+      )
+    }
+  </View>
+)
+
+const styles = StyleSheet.create({
+});
+
+const mapStateToProps = state => {
+  const { todos } = state;
+  return { todos };
+}
+
+export default connect(
+  mapStateToProps,
+  {
+		dispatchToggleTodo: toggleTodo,
+  }
+)(TodoList);
