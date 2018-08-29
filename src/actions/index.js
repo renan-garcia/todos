bd8b0dee08a9953cdfee65b2ccb587428bc1b143
Renan Garcia <email@renangarcia.me>
2018-08-29 11:25:15 -0300

    Cria reducers e actions para status de tarefa

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,9 @@ export const toggleTodo = todoId => ({
   type: TOGGLE_TODO,
   todoId
 });
+
+export const SET_TODO_TEXT = 'SET_TODO_TEXT';
+export const setTodoText = text => ({
+  type: SET_TODO_TEXT,
+  text
+});
